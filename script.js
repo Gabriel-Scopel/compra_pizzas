39429d97b7bb2aac29082228f9a399d3338be4b7
Gabriel-Scopel <gabriel.souza.scopel@hotmail.com>
2022-06-08 23:16:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const c=(el)=>document.querySelector(el);
-pizzaJson.map((item,index)=>{
-    let pizzaItem=c('.models.pizza-item').cloneNode(true);
-    c('.pizza-area').append(pizzaItem);
-})
-                                
-                                                      I
+
+const c=(el)=> document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem=c('.models .pizza-item').cloneNode(true);
+    c('.pizza-area').append(pizzaItem); 
+    
+});
+                          
